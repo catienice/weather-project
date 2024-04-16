b6b03c0ee1185408aff080c32ab01968e93afc6c
Catie <catienice@gmail.com>
2024-04-16 14:09:39 -0700

    first part done

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const submitButton = document.querySelector('.submit');
 const searchInput = document.querySelector('.input');
-const coordinates = [];
 
 
 submitButton.addEventListener('click', function(){
-  const city = document.querySelector('.input').value;
+  let city = document.querySelector('.input').value;
 
   const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=91c003f75e0552ff819bcfb19c839958`
+
+  document.querySelector('.input').value = ' ';
+  document.querySelector('.current-weather').innerHTML = ' ';
 
   // fetches coordinates and adds to coordinates array
   fetch(url, {
@@ -15,20 +17,16 @@ submitButton.addEventListener('click', function(){
   })
     .then(res => res.json())
     .then(data => {
-    coordinates.lat = data[0].lat
-    coordinates.lon = data[0].lon
-    console.log(coordinates.lat, coordinates.lon)
+    fetchDaily(data);
     })
+  
 })
 
 // fetch daily weather from API
-const fetchDaily = function() {
-  const lat = coordinates[0]
-  
-  
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat={${lat}}&lon={${lon}}&appid={91c003f75e0552ff819bcfb19c839958}`;
-
-  console.log(lat);
+const fetchDaily = function(data) {
+  let lat = data[0].lat
+  let lon = data[0].lon
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=91c003f75e0552ff819bcfb19c839958`;
 
   fetch(url, {
     method: 'GET',
@@ -37,12 +35,39 @@ const fetchDaily = function() {
   .then(data => data.json())
   .then(data => {
     console.log('fetch success', data);
+    kelvinConvert(data);
+    renderDailyWeather(data)
   })
+
 };
 
+// convert current weather
+const kelvinConvert = function (data) {
+  let kelvinTemp = data.main.temp
+  return Math.floor((kelvinTemp - 273.15) * 9/5 + 32)
+}
+
+// render daily weather
+const renderDailyWeather = function (data){
+  let currTemp = kelvinConvert(data);
+  let city = data.name;
+  let weather = data.weather[0].main;
+  let weatherIcon = data.weather[0].icon;
+  let template = `
+    <div class="column">
+    <h3>${currTemp}°<br>
+    ${city}<br>
+    ${weather}
+    </h3>
+    </div>
+    <div class="column">
+    <img src="https://openweathermap.org/img/wn/${weatherIcon}@2x.png">
+    </div>
+  `
+  document.querySelector('.current-weather').insertAdjacentHTML('beforeend', template);
+}
 
 // fetch forecast from API
 const fetchForecast = function(input) {
   
 };
-
